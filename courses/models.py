5072dcbe38c511264098e35bbb7a96c541c967d6
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-07-08 14:32:55 -0400

    Added some comments to models.py

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Student(models.Model):
+	# Feel free to change year abbreviations
     CLASS_YEAR = [
         ('fy', 'First year'),
         ('so', 'Sophomore'),
@@ -10,6 +11,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     class_year = models.CharField(max_length=5, choices=CLASS_YEAR)
+	# Currently commented out until Major foregin key is fully defined 
     #primary_major = models.ForeignKey(Major)
     #secondary_major = models.ForeignKey(Major)
     major_requirements_completed = models.BooleanField(default=False)
