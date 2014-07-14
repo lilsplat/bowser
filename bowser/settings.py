a8fc98851d4739efab8d49e2c94d1b81ad52029d
lilsplat <lilian@20-c9-d0-b8-29-a1.wpa.txstate.edu>
2014-07-14 15:38:11 -0500

    Debugging models.py
    Student -> added username, email fields; commented out add/remove
    course methods (don't work)
    Distribution -> added numcourses, added methods
    Course works
    Major works

--- bowser/settings.py
+++ bowser/settings.py
@@ -122,7 +122,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    # 'bowser',
     'courses',
     'south'
     )
--- courses/models.py
+++ courses/models.py
@@ -12,31 +12,101 @@ class Student(models.Model):
         (JUNIOR, 'Junior'),
         (SENIOR, 'Senior'),
         ]
+    username = models.CharField(max_length=200)#, primary_key=True)
+    email = models.EmailField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     class_year = models.CharField(max_length=5, choices=CLASS_YEAR, default=FIRSTYEAR)
     primary_major = models.ForeignKey('Major', related_name='primary major')
-    secondary_major = models.ForeignKey('Major', related_name='secondary major')
+    secondary_major = models.ForeignKey('Major', related_name='secondary major', null=True, blank=True) 
     major_requirements_completed = models.BooleanField(default=False)
     distribution_requirements_completed = models.BooleanField(default=False)
     gpa = models.IntegerField()
     courses = models.ManyToManyField('Course', through='Enrollment')
-   
-    def add_course(self, course):
-	""" Adds a course to a student's courses """
-        try:
-            self.courses.add(course)
-        except:
-            raise Exception('course could not be added.')
+ #    def add_course(self, course):
+	# """ Adds a course to a student's courses """
+ #        try:
+ #            self.courses.add(course)
+ #        except:
+ #            raise Exception('course could not be added.')
     
-    def remove_course(self, course):
-        try:
-            self.courses.remove(course)
-        except:
-            raise Exception('course could not be removed.')
+ #    def remove_course(self, course):
+ #        try:
+ #            self.courses.remove(course)
+ #        except:
+ #            raise Exception('course could not be removed.')
 
-	def __unicode__(self):
-		return self.first_name + ' ' + self.last_name	
+	# def __unicode__(self):
+	# 	return self.first_name + ' ' + self.last_name	
+
+
+class Distribution(models.Model):
+    AMTFV = "AMTFV"
+    EC = "EC"
+    HS = "HS"
+    LL = "LL"
+    MM = "MM"
+    NPS = "NPS"
+    QRB = "QRB"
+    QRF = "QRF"
+    REMP = "REMP"
+    SBA = "SBA"
+    NONE = "NONE"
+    LAB = "LAB"
+    FYS = "FYS"
+
+    DISTRIBUTIONS = [
+        (AMTFV, "Arts, Music, Theatre, Film, Video"),
+        (EC, "Epistemology and Cognition"),
+        (HS, "Historical Studies"),
+        (LL, "Language and Literature"),
+        (MM, "Mathematical Modeling"),
+        (NPS, "Natural and Physical Sciences"),
+        (QRB, "QRB"), #basic QR
+        (QRF, "QRF"), #QR requirement
+        (REMP, "Religion, Ethics, and Moral Philosophy"),
+        (SBA, "Social and Behavioral Analysis"),
+        (NONE, "None"),
+        (LAB, "Lab"),
+        (FYS, "First Year Seminar"),
+    ]
+
+    name = models.CharField(max_length=5, choices=DISTRIBUTIONS, default=NONE)
+    num_courses = models.IntegerField(default=0)
+    #courses ok can be accessed by Distribution.course_set
+
+
+    def is_fulfilled_by(self, course):
+        """Returns if a course counts toward the Distribution"""
+        if self.course_set.filter(name__contains=course.name).count() > 0:
+            return True
+        else:
+            return False
+
+    def num_courses_togo(self, courses):
+        """Returns the number of courses left to take in the Distribution, given a list of Courses"""
+        num_togo = self.num_courses
+        for course in courses:
+            if self.is_fulfilled_by(course) == True:
+                num_togo -= 1
+        return num_togo
+
+    def suggested_courses(self, courses):
+        """Returns a list of suggested courses to fulfill the Distribution, given a list of Courses"""
+        #additional functions: should compensate for fall/spring availability
+        suggestions = Distributions.course_set.all() #all available courses
+        for course in courses:
+            if self.is_fulfilled_by(course):
+                suggestions.remove(course)
+        return suggestions
+
+    def is_completed(self, courses):
+        """Returns if the distribution is complete, based on the given list of Courses"""
+        if self.num_courses_togo(courses) == 0:
+            return True
+        else:
+            return False
+
 
 class Course(models.Model):
     code = models.CharField(max_length=200) #i.e. CS110
@@ -45,8 +115,9 @@ class Course(models.Model):
     date = models.CharField(max_length=200) #i.e. TF
     prof = models.CharField(max_length=200)
     prof_site = models.CharField(max_length=200) #link to professor's website (comes on course browser)
-    dist = models.ForeignKey('Distribution', related_name='distribution')
     comments = models.TextField()
+    dists = models.ManyToManyField(Distribution)
+
     #students can be accessed through Course.student_set
 
     def conflicts(self, other_course):
@@ -176,87 +247,21 @@ class Major(models.Model):
     ]
 
     name = models.CharField(max_length=5, choices=MAJORS, default=UND)
+
     # courses = models.ManyToManyField(Course, through='Major_Requirements') 
     #^^^^^^ need to change
 
-class Distribution_Requirement(models.Model):
-    AMTFV_LL = "AMTFV_LL"
-    EC_HS_REMP = "EC_HS_REMP"
-    SBA = "SBA"
-    MM_NPS = "MM_NPS"
+# class Distribution_Requirement(models.Model):
+#     AMTFV_LL = "AMTFV_LL"
+#     EC_HS_REMP = "EC_HS_REMP"
+#     SBA = "SBA"
+#     MM_NPS = "MM_NPS"
 
-    name = models.CharField(max_length=200)
-    dists = models.ManyToManyField('Distribution')
+#     name = models.CharField(max_length=200)
+#     dists = models.ManyToManyField('Distribution')
 
-    def is_fulfilled_by(self, courses):
-		pass
-
-class Distribution(models.Model):
-    AMTFV = "AMTFV"
-    EC = "EC"
-    HS = "HS"
-    LL = "LL"
-    MM = "MM"
-    NPS = "NPS"
-    QRB = "QRB"
-    QRF = "QRF"
-    REMP = "REMP"
-    SBA = "SBA"
-    NONE = "NONE"
-    LAB = "LAB"
-    FYS = "FYS"
-
-    DISTRIBUTIONS = [
-        (AMTFV, "Arts, Music, Theatre, Film, Video"),
-        (EC, "Epistemology and Cognition"),
-        (HS, "Historical Studies"),
-        (LL, "Language and Literature"),
-        (MM, "Mathematical Modeling"),
-        (NPS, "Natural and Physical Sciences"),
-        (QRB, "QRB"), #basic QR
-        (QRF, "QRF"), #QR requirement
-        (REMP, "Religion, Ethics, and Moral Philosophy"),
-        (SBA, "Social and Behavioral Analysis"),
-        (NONE, "None"),
-        (LAB, "Lab"),
-        (FYS, "First Year Seminar"),
-    ]
-
-    name = models.CharField(max_length=5, choices=DISTRIBUTIONS, default=NONE)
-    # num_courses = models.IntegerField() #number of courses needed for this distribution
-
-    #courses ok can be accessed by Distribution.course_set
-
-    def is_fulfilled_by(self, course):
-        """Returns if a course counts toward the Distribution"""
-        if self.course_set.filter(name__contains=course.name).count() > 0:
-            return True
-        else:
-            return False
-
-    # def num_courses_togo(self, courses):
-    #     """Returns the number of courses left to take in the Distribution, given a list of Courses"""
-    #     num_togo = num_courses
-    #     # for course in courses:
-    #     #     if self.is_fulfilled_by(course) == true:
-    #     #         num_togo -= 1
-    #     return num_togo
-
-    # def suggested_courses(self, courses):
-    #     #additional functions: should compensate for fall/spring availability
-    #     suggestions = Distributions.course_set.all() #all available courses
-    #     for course in courses:
-    #         if self.is_fulfilled_by(course):
-    #             suggestions.remove(course)
-    #     return suggestions
-
-    # def is_completed(self, courses):
-    #     """Returns if the distribution is complete, based on the given list of Courses"""
-    #     if self.num_courses_togo(courses) == 0:
-    #         return True
-    #     else:
-    #         return False
-
+  #   def is_fulfilled_by(self, courses):
+		# pass
 
 
 
