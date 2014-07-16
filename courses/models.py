23a1fd08b46cadee7c2a6915d423d4358b55b768
lilsplat <lilian@20-c9-d0-b8-29-a1.wpa.txstate.edu>
2014-07-16 11:08:50 -0500

    Modified Student.distributions_todo
    - Created 3 separate requirement categories (language, qrbasic,
    distribution) to fit student needs (i.e some students tested out of
    language, qrbasic, etc)
    - Modified DIST_REQ to represent all 3 distribution areas
    - Added fields qrb_passed and foreign_lang_passed to Student
    - Created new distributions_todo method

--- courses/models.py
+++ courses/models.py
@@ -19,22 +19,47 @@ SBA = "SBA"
 NONE = "NONE"
 LAB = "LAB"
 FYS = "FYS"
+W = "W" #fyw
+LANG100 = "LANG100"
+LANG200 = "LANG200"
+LL_AMTFV = "LL_AMTFV"
+EC_HS = "EC_HS"
+HS_REMP = "HS_REMP"
+MM_NPS = "MM_NPS"
 
-DISTRIBUTIONS = [
-	(AMTFV, "Arts, Music, Theatre, Film, Video"),
-    (EC, "Epistemology and Cognition"),
-    (HS, "Historical Studies"),
+#multicultural requirement??
+
+LANG_REQ = [
+    #------------can have overlap--------------------
+    (LANG100, "Foreign Language 100L"), #perhaps to increase simplicity should these just include 
+    (LANG100, "Foreign Language 100L"), #the ability to be fulfilled by 'passed the language req'?
+    (LANG200, "Foreign Language 200L"),
+    (LANG200, "Foreign Language 200L")
+]
+
+QRB_REQ = [
+    (QRB, "Basic QR")
+]
+
+DIST_REQ = [
+    #------------can have overlap--------------------
+    (QRF, "QR Overlay"), #QR requirement
+    (LAB, "Lab"),
+    #------------------------------------------------
+    (W, "First Year Writing"),
+    #------------humanities dist area-----------------
     (LL, "Language and Literature"),
+    (AMTFV, "Arts, Music, Theater, Film and Video"),
+    (LL_AMTFV, "Language and Literature or Arts, Music, Theater, Film and Video"),
+    #-----------social sciences dist area------------
+    (SBA, "Social and Behavioral Analysis"),
+    (EC_HS, "Epistemology and Cognition or Historical Studies"),
+    (HS_REMP, "Historical Studies or Religion, Ethics, and Moral Philosophy"),
+    #-----------math and science dist area------------
     (MM, "Mathematical Modeling"),
     (NPS, "Natural and Physical Sciences"),
-    (QRB, "QRB"), #basic QR
-    (QRF, "QRF"), #QR requirement
-    (REMP, "Religion, Ethics, and Moral Philosophy"),
-    (SBA, "Social and Behavioral Analysis"),
-    (NONE, "None"),
-    (LAB, "Lab"),
-    (FYS, "First Year Seminar"),
-]
+    (MM_NPS, "Mathematical Modeling or Natural and Physical Sciences"),
+ ]
 
 class Student(models.Model):
     FIRSTYEAR = 'fy'
@@ -60,6 +85,9 @@ class Student(models.Model):
     gpa = models.IntegerField()
     courses = models.ManyToManyField('Course', through='Enrollment')
 
+    qrb_passed = models.BooleanField(default=False) #if they passed the QR assessment
+    foreign_lang_passed = models.BooleanField(default=False) #if they passed the foreign lang requirement
+
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
 	
@@ -84,14 +112,68 @@ class Student(models.Model):
 		)
 		enrollment.delete()
 	
-	def distributions_todo(self):
-	# I'm thinking Distributions, courses and all our long lists will be in a SQL table
-	# So we don't have to have these lists as global vars
-		distributions_list = DISTRIBUTIONS
-		for distribution in distributions_list:
-			if not is_completed(distribution, self.courses):
-				distributions_list.remove(distribution)
-		return distributions_list
+	# def distributions_todo(self):
+	# # I'm thinking Distributions, courses and all our long lists will be in a SQL table
+	# # So we don't have to have these lists as global vars
+	# 	distributions_list = DISTRIBUTIONS
+	# 	for distribution in distributions_list:
+	# 		if not is_completed(distribution, self.courses):
+	# 			distributions_list.remove(distribution)
+	# 	return distributions_list
+
+    def distributions_todo(self):
+        distribution_list = DIST_REQ
+        num_overlap = 2 #default num 'overlap' courses i.e. courses that  can count for multiple majors; always at head of list
+
+        #modify distribution_list to suit Student
+        """not sure if you can do this comparison...field may return 1 or 0 instead of True or False"""
+        if foreign_lang_passed == False:
+            distribution_list = LANG_REQ + distribution_list
+            num_overlap += 4 #adding 4 lang classes to the overlap category at head of list
+        if qrb_passed == False:
+            distribution_list = distribution_list + QRB_REQ
+
+        #copy Student's courses
+        courses_taken = self.courses.all()
+
+        #check fulfillment of all "overlap" dist
+        for distribution in distribution_list[:num_overlap]:
+            for course in courses_taken:
+                if distribution.is_fulfilled_by(course):
+                    # courses_taken.remove(course) no need to do this b/c it is overlap dist
+                    distribution_list.remove(distribution)
+                    num_overlap -= 1
+
+        """NB: does not yet account for courses w/ multiple dists"""
+        #check fulfillment of all other dists
+        for distribution in distribution_list[num_overlap:]:
+            for course in courses_taken:
+                if distribution.is_fulfilled_by(course):
+                    courses_taken.remove(course)
+                    distribution_list.remove(distribution)
+
+        return distribution_list
+
+        """Possible solution to account for multiple dists commented out below"""
+        # #first loop: check fulfillment of all dists, first 'fitting' all courses with only 1 distribution
+        # #to a distribution
+        # for distribution in distribution_list[num_overlap:]:
+        #     for course in courses_taken:
+        #         if distribution.is_fulfilled_by(course):
+        #             if course.dists.count() > 1: #if course has multiple dists
+        #                 #do nothing, table this course
+        #             else:
+        #                 courses_taken.remove(course)
+        #                 distribution_list.remove(distribution)
+
+        # #second loop: arbitrarily assign all multi-dist courses to the first dist they fit
+        # for distribution in distribution_list[num_overlap:]:
+        #     for course in courses_taken:
+        #         if distribution.is_fulfilled_by(course):
+        #             courses_taken.remove(course)
+        #             distribution_list.remove(distribution)
+
+
 	
 	def major_todo(self):
 		major_course_list = primary_major.major_courses
