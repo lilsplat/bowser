dd22bd1f8718c729ba2ed366a0ff7a7364160bd3
lilsplat <lilian@20-c9-d0-b8-29-a1.wpa.txstate.edu>
2014-07-17 13:32:24 -0500

    Updating tests

--- courses/tests.py
+++ courses/tests.py
@@ -7,64 +7,60 @@ Replace this with more appropriate tests for your application.
 
 from django.utils import unittest
 from django.test import TestCase
-from courses.models import *
+from courses.models import Course, Student, Distribution, Major
 import datetime
-
-cs = Major.objects.create(name='CS')
-
-# class DistributionTester(TestCase):
-# 	def setUp(self):
 
 
 class StudentTester(TestCase):
+	fixtures = ['initial_data_dump.json']
+
 	def setUp(self):
-		# cs = Major.objects.create(name='CS')
 		Student.objects.create(
 			email='lxie@wellesley.edu',
 			first_name='lily',
 			last_name='xie',
 			class_year='ju',
-			primary_major=cs,
+			primary_major=Major.objects.get(name='Computer Science'),
 			gpa=1.0
 			)
 		Student.objects.create(
 			first_name='sravanti',
 			last_name='tekumalla',
 			# class_year=''
-			primary_major=Major.objects.create(name='CS'),
+			primary_major=Major.objects.get(name='Computer Science'),
 			gpa=1.0
 			)
-		Course.objects.create(code='cs111',name='Intro CS',time='1:00',
-			date='MF',prof='rhys',prof_site='blah')
-		Course.objects.create(code='soc100',name='Intro Soc',time='1:00',
-			date='MF',prof='x',prof_site='blah')
-
 
 	def test(self):
 		lily = Student.objects.get(first_name='lily')
 		print lily.email
 		print lily.email.split('@')[0]
 		print lily.username
+		print lily.primary_major
 		self.assertEqual('lily', lily.first_name)
 		self.assertEqual('xie', lily.last_name)
 		self.assertEqual('ju', lily.class_year)
-		# self.assertEqual(Major.objects.create(name='CS'), lily.primary_major)
-
 
 		sravanti = Student.objects.get(first_name='sravanti')
-		cs111 = Course.objects.get(code='cs111')
-		soc100 = Course.objects.get(code='soc100')
+		# cs111 = Course.objects.filter(code__startswith='CS111')
+		# print cs111
+		# soc100 = Course.objects.filter(code__startswith='SOC100')
 
-		e1 = Enrollment.objects.create(student=lily, course=cs111, date_taken=datetime.date.today(), rating=5)
-		print e1.date_taken
+		# e1 = Enrollment.objects.create(student=lily, course=cs111, date_taken=datetime.date.today(), rating=5)
+		# print e1.date_taken
 
-		e2 = Enrollment.objects.create(student=sravanti,course=cs111,date_taken=datetime.date.today(),rating=5)
+		# e2 = Enrollment.objects.create(student=sravanti,course=cs111,date_taken=datetime.date.today(),rating=5)
 
 		# print lily.courses
 		# print cs111.student_set.all()
 
+		print Course.objects.all()
+
 
 class DistributionTester(TestCase):
+	fixtures = ['initial_data_dump.json']
+
+
 	def setUp(self):
 		Distribution.objects.create(name='mm')
 		Distribution.objects.create(name='sba')
@@ -88,7 +84,5 @@ class DistributionTester(TestCase):
 		self.assertFalse(mm.is_fulfilled_by(soc100))
 		self.assertFalse(sba.is_fulfilled_by(cs111))
 
-		print mm.num_courses
-		print mm.num_courses_togo(courselist)
 
 
