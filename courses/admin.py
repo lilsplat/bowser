8a1545fdb12bc9b4e444637b91b2afe4ce5c52e6
lilsplat <lilian@a8-20-66-08-59-fe.resnet.txstate.edu>
2014-07-19 00:35:57 -0500

    Merge branch 'master' of https://github.com/lilsplat/bowser

---
+++ courses/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from courses.models import UserProfile
+
+admin.site.register(UserProfile)
+
---
+++ courses/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from courses import views
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bowser.views.home', name='home'),
+    # url(r'^bowser/', include('bowser.urls')),
+    #url(r'^$', TemplateView.as_view(template_name='index.html')),
+    #(r'^$', index),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', views.index, name='index'),
+	url(r'^register/$', views.register, name='register'),
+)
