43fdf0dafa79954be7fa15ecc53a5346182ef494
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-07-19 15:10:48 -0400

    Can now view students in admin console.

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from courses.models import UserProfile
+from courses.models import UserProfile, Student
 
 admin.site.register(UserProfile)
-
+admin.site.register(Student)
