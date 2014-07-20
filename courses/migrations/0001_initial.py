5af62f1fbe4c04cd14c211d79591558d575acdfd
lilsplat <lilian@a8-20-66-08-59-fe.resnet.txstate.edu>
2014-07-19 21:29:58 -0500

    Populated courses_course_dists

--- courses/migrations/0001_initial.py
+++ courses/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -8,13 +8,222 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Distribution'
+        db.create_table(u'courses_distribution', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='NONE', max_length=200)),
+        ))
+        db.send_create_signal(u'courses', ['Distribution'])
+
+        # Adding model 'Course'
+        db.create_table(u'courses_course', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('code', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('dept', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('time', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('date', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('prof', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('prof_site', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('comments', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'courses', ['Course'])
+
+        # Adding M2M table for field dists on 'Course'
+        m2m_table_name = db.shorten_name(u'courses_course_dists')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('course', models.ForeignKey(orm[u'courses.course'], null=False)),
+            ('distribution', models.ForeignKey(orm[u'courses.distribution'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['course_id', 'distribution_id'])
+
+        # Adding model 'Student'
+        db.create_table(u'courses_student', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('class_year', self.gf('django.db.models.fields.CharField')(default='fy', max_length=200)),
+            ('primary_major', self.gf('django.db.models.fields.related.ForeignKey')(related_name='primary major', null=True, to=orm['courses.Major'])),
+            ('secondary_major', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='secondary major', null=True, to=orm['courses.Major'])),
+            ('major_requirements_completed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('distribution_requirements_completed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('gpa', self.gf('django.db.models.fields.FloatField')(default=2.0, null=True)),
+            ('qrb_passed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('foreign_lang_passed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'courses', ['Student'])
+
+        # Adding model 'Course_Bucket'
+        db.create_table(u'courses_course_bucket', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal(u'courses', ['Course_Bucket'])
+
+        # Adding M2M table for field courses on 'Course_Bucket'
+        m2m_table_name = db.shorten_name(u'courses_course_bucket_courses')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('course_bucket', models.ForeignKey(orm[u'courses.course_bucket'], null=False)),
+            ('course', models.ForeignKey(orm[u'courses.course'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['course_bucket_id', 'course_id'])
+
+        # Adding model 'Major'
+        db.create_table(u'courses_major', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(default='Undecided', max_length=200)),
+        ))
+        db.send_create_signal(u'courses', ['Major'])
+
+        # Adding model 'Enrollment'
+        db.create_table(u'courses_enrollment', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('student', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.Student'])),
+            ('course', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.Course'])),
+            ('date_taken', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
+            ('rating', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'courses', ['Enrollment'])
+
+        # Adding unique constraint on 'Enrollment', fields ['student', 'course']
+        db.create_unique(u'courses_enrollment', ['student_id', 'course_id'])
+
+        # Adding model 'UserProfile'
+        db.create_table(u'courses_userprofile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('email_verified', self.gf('django.db.models.fields.BooleanField')()),
+        ))
+        db.send_create_signal(u'courses', ['UserProfile'])
+
 
     def backwards(self, orm):
-        pass
+        # Removing unique constraint on 'Enrollment', fields ['student', 'course']
+        db.delete_unique(u'courses_enrollment', ['student_id', 'course_id'])
+
+        # Deleting model 'Distribution'
+        db.delete_table(u'courses_distribution')
+
+        # Deleting model 'Course'
+        db.delete_table(u'courses_course')
+
+        # Removing M2M table for field dists on 'Course'
+        db.delete_table(db.shorten_name(u'courses_course_dists'))
+
+        # Deleting model 'Student'
+        db.delete_table(u'courses_student')
+
+        # Deleting model 'Course_Bucket'
+        db.delete_table(u'courses_course_bucket')
+
+        # Removing M2M table for field courses on 'Course_Bucket'
+        db.delete_table(db.shorten_name(u'courses_course_bucket_courses'))
+
+        # Deleting model 'Major'
+        db.delete_table(u'courses_major')
+
+        # Deleting model 'Enrollment'
+        db.delete_table(u'courses_enrollment')
+
+        # Deleting model 'UserProfile'
+        db.delete_table(u'courses_userprofile')
+
 
     models = {
-        
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'courses.course': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Course'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'comments': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'dept': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'dists': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['courses.Distribution']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'prof': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'prof_site': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'time': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'courses.course_bucket': {
+            'Meta': {'object_name': 'Course_Bucket'},
+            'courses': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['courses.Course']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'courses.distribution': {
+            'Meta': {'object_name': 'Distribution'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'NONE'", 'max_length': '200'})
+        },
+        u'courses.enrollment': {
+            'Meta': {'unique_together': "[('student', 'course')]", 'object_name': 'Enrollment'},
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['courses.Course']"}),
+            'date_taken': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'rating': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'student': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['courses.Student']"})
+        },
+        u'courses.major': {
+            'Meta': {'object_name': 'Major'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'Undecided'", 'max_length': '200'})
+        },
+        u'courses.student': {
+            'Meta': {'object_name': 'Student'},
+            'class_year': ('django.db.models.fields.CharField', [], {'default': "'fy'", 'max_length': '200'}),
+            'courses': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['courses.Course']", 'null': 'True', 'through': u"orm['courses.Enrollment']", 'symmetrical': 'False'}),
+            'distribution_requirements_completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'foreign_lang_passed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'gpa': ('django.db.models.fields.FloatField', [], {'default': '2.0', 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'major_requirements_completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'primary_major': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'primary major'", 'null': 'True', 'to': u"orm['courses.Major']"}),
+            'qrb_passed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'secondary_major': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'secondary major'", 'null': 'True', 'to': u"orm['courses.Major']"}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
+        u'courses.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'email_verified': ('django.db.models.fields.BooleanField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        }
     }
 
     complete_apps = ['courses']
