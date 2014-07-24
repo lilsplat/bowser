cde0f5c1642806d3681e70c0828c1f67aca897cf
lilsplat <lilian@a8-20-66-08-59-fe.resnet.txstate.edu>
2014-07-23 21:04:14 -0500

    Editing spider to pull addit. course fields

--- scrapy/bowserbot/spiders/browser_spider.py
+++ scrapy/bowserbot/spiders/browser_spider.py
@@ -1,4 +1,4 @@
-#course_spider.py
+#browser_spider.py
 
 import scrapy
 from scrapy.selector import Selector 
@@ -7,13 +7,14 @@ from bowserbot.items import *
 from courses.models import *
 
 
-class CourseSpider(scrapy.Spider):
-    name = "course_spider"
-    # allowed_domains = ["https://courses.wellesley.edu"]
+class BrowserSpider(scrapy.Spider):
+    name = "browser_spider"
+    allowed_domains = ["https://courses.wellesley.edu"]
 
     start_urls = [
     #change from /tutorial to /bowserbot
-        "file:///Users/lilian/Documents/2014/GitHub/bowser/scrapy/bowserbot/course_browser_htmls/201409.txt", 
+        "https://courses.wellesley.edu/display_single_course_cb.php?crn=13275&semester=201409&pe_term=&skip_graphics=1&no_navs=1"
+        # "file:///Users/lilian/Documents/2014/GitHub/bowser/scrapy/bowserbot/course_browser_htmls/201409.txt", 
         # "file:///Users/lilian/Documents/2014/GitHub/bowser/scrapy/bowserbot/course_browser_htmls/201407", 
         # "file:///Users/lilian/Documents/2014/GitHub/bowser/scrapy/bowserbot/course_browser_htmls/201406.txt", 
         # "file:///Users/lilian/Documents/2014/GitHub/bowser/scrapy/bowserbot/course_browser_htmls/201402.txt", 
@@ -90,162 +91,192 @@ class CourseSpider(scrapy.Spider):
 
 
     def parse(self, response):
-
         self.log('A response from %s just arrived!' % response.url)
-
-        #parsing file:///.... into the semester code
-        path = response.url
-        code = path.split("/").pop(12)
-        code = code.split(".").pop(0)
-
-        filewriter = open('course_browser_database/' + code + '_new.txt', 'w')
-
-
-        for sel in response.xpath('//tbody/tr'): #each <tr> is a class table
-            site=sel.xpath('th[2]/a/@href').extract()
-
-            course = sel.xpath('th[2]/a/text()').extract()
-            course = self.test_and_pop(course)
-            course = course.replace(" ","")
-
-            title = sel.xpath('th[3]/text()').extract()
-            title = self.test_and_pop(title)
-
-            time = sel.xpath('th[7]/text()').extract() #has empty and multiple object lists
-            time = self.test_and_pop(time)
-
-            date = sel.xpath('th[8]/text()').extract() #has empty and multiple object lists
-            date = self.test_and_pop(date)
-            date = date.strip() #idk why but new line?
-
-            prof = sel.xpath('th[9]/a/text()').extract() #has empty lists
-            prof = self.test_and_pop(prof)
-
-            prof_site = sel.xpath('th[9]/a/@href').extract() #has empty lists
-            prof_site = self.test_and_pop(prof_site)
-            prof_site = 'https://courses.wellesley.edu/' + prof_site
-
-            distribution = sel.xpath('th[11]/text()').extract() #has multiple object lists
-            distribution = self.test_and_pop(distribution)
-
-
-            # """ Do we need to encode UTF-8? """
-
-            # c = DjangoCourseItem()
-
-            # c['code'] = course.encode("UTF-8")
-            # c['name'] = title.encode("UTF-8")
-            # c['time'] = time.encode("UTF-8")
-            # c['date'] = date.encode("UTF-8")
-            # c['prof'] = prof.encode("UTF-8")
-            # c['prof_site'] = prof_site.encode("UTF-8")
-            # return c
+        for sel in response.xpath('//tbody'):
+            course=sel.xpath('tr[1]/th[2]/text()').extract()
+            crn=sel.xpath('tr[2]/th[2]/text()').extract()
+            title=sel.xpath('tr[3]/th[2]/text()').extract()
+            credit_hours=sel.xpath('tr[4]/th[2]/text()').extract()
+            description=sel.xpath('tr[5]/th[2]/text()').extract()
+            seats_available=sel.xpath('tr[6]/th[2]/text()').extract()
+            max_enrollment=sel.xpath('tr[7]/th[2]/text()').extract()
+            by_permission=sel.xpath('tr[8]/th[2]/text()').extract()
+            prereq=sel.xpath('tr[9]/th[2]/text()').extract()
+            dist=sel.xpath('tr[10]/th[2]/text()').extract()
+            notes=sel.xpath('tr[11]/th[2]/text()').extract()
+            prof=sel.xpath('tr[12]/th[2]/text()').extract()
+            time_and_date=sel.xpath('tr[13]/th[2]/text()').extract()
             
-            # dist = []
-            # if len(distribution) == 0:
-            #     d = DjangoDistItem()
-            #     d['name'] = 'None assigned'
-            #     dist.append(d)
-            # elif len(distribution) == 1:
-            #     d = DjangoDistItem()
-            #     d['name'] = distribution.pop().strip().encode("UTF-8")
-            #     dist.append(d) 
-            # else:
-            #     for element in distribution:
-            #         d = DjangoDistItem()
-            #         d['name'] = element.strip().encode("UTF-8")
-            #         dist.append(d)
+            print course
+            print crn
+            print title
+            print credit_hours
+            print description
+            print seats_available
+            print max_enrollment
+            print by_permission
+            print prereq
+            print dist
+            print notes
+            print time_and_date
+            print 'end'
 
 
-            # """ TO DO: fix this!! django does not recognize DjangoDistItem as an acceptable input to the dists field """
+"""COMMENTED OFF STUFF FROM COURSE_SPIDER BELOW"""
 
-            # test = dist[0]
-            # c['dists'] = test
+    #     self.log('A response from %s just arrived!' % response.url)
+
+    #     #parsing file:///.... into the semester code
+    #     path = response.url
+    #     code = path.split("/").pop(12)
+    #     code = code.split(".").pop(0)
+
+    #     filewriter = open('course_browser_database/' + code + '_new.txt', 'w')
+
+
+    #     for sel in response.xpath('//tbody/tr'): #each <tr> is a class table
+
+    #         course = sel.xpath('th[2]/a/text()').extract()
+    #         course = self.test_and_pop(course)
+    #         course = course.replace(" ","")
+
+    #         title = sel.xpath('th[3]/text()').extract()
+    #         title = self.test_and_pop(title)
+
+    #         time = sel.xpath('th[7]/text()').extract() #has empty and multiple object lists
+    #         time = self.test_and_pop(time)
+
+    #         date = sel.xpath('th[8]/text()').extract() #has empty and multiple object lists
+    #         date = self.test_and_pop(date)
+    #         date = date.strip() #idk why but new line?
+
+    #         prof = sel.xpath('th[9]/a/text()').extract() #has empty lists
+    #         prof = self.test_and_pop(prof)
+
+    #         prof_site = sel.xpath('th[9]/a/@href').extract() #has empty lists
+    #         prof_site = self.test_and_pop(prof_site)
+    #         prof_site = 'https://courses.wellesley.edu/' + prof_site
+
+    #         distribution = sel.xpath('th[11]/text()').extract() #has multiple object lists
+    #         distribution = self.test_and_pop(distribution)
+
+
+    #         # """ Do we need to encode UTF-8? """
+
+    #         # c = DjangoCourseItem()
+
+    #         # c['code'] = course.encode("UTF-8")
+    #         # c['name'] = title.encode("UTF-8")
+    #         # c['time'] = time.encode("UTF-8")
+    #         # c['date'] = date.encode("UTF-8")
+    #         # c['prof'] = prof.encode("UTF-8")
+    #         # c['prof_site'] = prof_site.encode("UTF-8")
+    #         # return c
+            
+    #         # dist = []
+    #         # if len(distribution) == 0:
+    #         #     d = DjangoDistItem()
+    #         #     d['name'] = 'None assigned'
+    #         #     dist.append(d)
+    #         # elif len(distribution) == 1:
+    #         #     d = DjangoDistItem()
+    #         #     d['name'] = distribution.pop().strip().encode("UTF-8")
+    #         #     dist.append(d) 
+    #         # else:
+    #         #     for element in distribution:
+    #         #         d = DjangoDistItem()
+    #         #         d['name'] = element.strip().encode("UTF-8")
+    #         #         dist.append(d)
+
+
+    #         # """ TO DO: fix this!! django does not recognize DjangoDistItem as an acceptable input to the dists field """
+
+    #         # test = dist[0]
+    #         # c['dists'] = test
 
             
 
 
 
-            print 'beginning writing\n'
-            print site
-            # print 'writing crn %s' % str(crn)
-            # filewriter.write(crn.encode("UTF-8") + '\n')
-            # print 'writing title %s' % title
-            # filewriter.write(title.encode("UTF-8") + '\n') #why?????
-            # print 'writing time %s' % str(time)
-            # filewriter.write(time.encode("UTF-8") + '\n')
-            # print 'writing date %s' % str(date)
-            # filewriter.write(date.encode("UTF-8") + '\n')
-            # print 'writing prof %s' % str(prof)
-            # filewriter.write(prof.encode("UTF-8") + '\n')
-            # print 'writing prof site %s' % str(prof_site)
-            # filewriter.write(prof_site.encode("UTF-8") + '\n')
-            # print 'writing distribution %s' % str(distribution)
-            # filewriter.write(distribution.encode("UTF-8") + '\n')
-            # print 'writing delimiter'
-            # filewriter.write("..")
+    #         # print 'beginning writing\n'
+    #         # print 'writing crn %s' % str(crn)
+    #         # filewriter.write(crn.encode("UTF-8") + '\n')
+    #         # print 'writing title %s' % title
+    #         # filewriter.write(title.encode("UTF-8") + '\n') #why?????
+    #         # print 'writing time %s' % str(time)
+    #         # filewriter.write(time.encode("UTF-8") + '\n')
+    #         # print 'writing date %s' % str(date)
+    #         # filewriter.write(date.encode("UTF-8") + '\n')
+    #         # print 'writing prof %s' % str(prof)
+    #         # filewriter.write(prof.encode("UTF-8") + '\n')
+    #         # print 'writing prof site %s' % str(prof_site)
+    #         # filewriter.write(prof_site.encode("UTF-8") + '\n')
+    #         # print 'writing distribution %s' % str(distribution)
+    #         # filewriter.write(distribution.encode("UTF-8") + '\n')
+    #         # print 'writing delimiter'
+    #         # filewriter.write("..")
 
-            # filewriter.write(#crn.encode("UTF-8") + "\n" + 
-            #     course.encode("UTF-8") + "\n" + 
-            #     title.encode("UTF-8") + "\n" + 
-            #     # str(isCourseFull) + "\n" + 
-            #     time.encode("UTF-8") + "\n" + 
-            #     date.encode("UTF-8") + "\n" + 
-            #     prof.encode("UTF-8") + "\n" + 
-            #     prof_site.encode("UTF-8") + "\n" + 
-            #     distribution.encode("UTF-8") + 
-            #     "\n#\n") #delimiter
+    #         filewriter.write(#crn.encode("UTF-8") + "\n" + 
+    #             course.encode("UTF-8") + "\n" + 
+    #             title.encode("UTF-8") + "\n" + 
+    #             # str(isCourseFull) + "\n" + 
+    #             time.encode("UTF-8") + "\n" + 
+    #             date.encode("UTF-8") + "\n" + 
+    #             prof.encode("UTF-8") + "\n" + 
+    #             prof_site.encode("UTF-8") + "\n" + 
+    #             distribution.encode("UTF-8") + 
+    #             "\n#\n") #delimiter
 
-        filewriter.close()
+    #     filewriter.close()
 
-    def test_and_pop(self, extracted_list):
-        if len(extracted_list) == 0:
-            return 'None assigned'
-        elif len(extracted_list) == 1:
-            return extracted_list.pop()
-        else:
-            string = extracted_list.pop().strip()
-            for element in extracted_list:
-                string = element.strip() + ',' + string
-            # print string
-            return string
+    # def test_and_pop(self, extracted_list):
+    #     if len(extracted_list) == 0:
+    #         return 'None assigned'
+    #     elif len(extracted_list) == 1:
+    #         return extracted_list.pop()
+    #     else:
+    #         string = extracted_list.pop().strip()
+    #         for element in extracted_list:
+    #             string = element.strip() + ',' + string
+    #         # print string
+    #         return string
         
 
 
 
 
     	
-    	   # page_title = response.selector.xpath('head/title/text()').extract()
-    	   # filename = "tutorial/txt_docs/" + str(page_title.pop()) + ".txt" #saved in txt docs directory
-    	   # f = open (filename, 'w') #new file writer with name of the title
+    # 	   # page_title = response.selector.xpath('head/title/text()').extract()
+    # 	   # filename = "tutorial/txt_docs/" + str(page_title.pop()) + ".txt" #saved in txt docs directory
+    # 	   # f = open (filename, 'w') #new file writer with name of the title
 
-        #    #creates selector to loop through all <p> tags for hyperlinks
-        #    for sel in response.xpath('//p'):
-        #     title = sel.xpath('a[contains(@href,"/wiki/")]/@title').extract() 
+    #     #    #creates selector to loop through all <p> tags for hyperlinks
+    #     #    for sel in response.xpath('//p'):
+    #     #     title = sel.xpath('a[contains(@href,"/wiki/")]/@title').extract() 
 
-        #    #problem is this only works with /a; it doesn't work on disambiguation pages because those use /link
-        #    #find wikipedia links and extract their titles
-	       #  #??????? what's the proper syntax here
-	       #  while not not title: #len(title) != 0:
-	       #  	f.write(str(title.pop()) + "\n") #write titles line by line onto file
+    #     #    #problem is this only works with /a; it doesn't work on disambiguation pages because those use /link
+    #     #    #find wikipedia links and extract their titles
+	   #     #  #??????? what's the proper syntax here
+	   #     #  while not not title: #len(title) != 0:
+	   #     #  	f.write(str(title.pop()) + "\n") #write titles line by line onto file
 
-	       #  #NB: it will scrape ALL //p tags even if they are empty. seems like a waste
+	   #     #  #NB: it will scrape ALL //p tags even if they are empty. seems like a waste
 
 
 
     
 
 
-    # def parse(self, response):
-    #     filename = response.url.split("/")[-2]
-    #     with open(filename, 'wb') as f:
-    #         f.write(response.body)
+    # # def parse(self, response):
+    # #     filename = response.url.split("/")[-2]
+    # #     with open(filename, 'wb') as f:
+    # #         f.write(response.body)
 
-    # def make_requests_from_url(url):
-    #     return scrapy.http.Request(url)
-    #     # return [scrapy.FormRequest("http://www.example.com/login",
-    #     #     formdata={'user': 'john', 'pass': 'secret'},
-    #     #     callback=self.logged_in)]
+    # # def make_requests_from_url(url):
+    # #     return scrapy.http.Request(url)
+    # #     # return [scrapy.FormRequest("http://www.example.com/login",
+    # #     #     formdata={'user': 'john', 'pass': 'secret'},
+    # #     #     callback=self.logged_in)]
 
 
 
