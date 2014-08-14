45eeaad723ab3a4ff5256c33bc717947d7e585d4
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-08-14 11:11:17 -0400

    Added prof reviews to course page

--- bowser/static/scripts/checklist.js
+++ bowser/static/scripts/checklist.js
@@ -7,10 +7,18 @@ $(document).ready(function(){
 	//	$('#completed_dists').click(function() {
         });
 	$('#incomplete_dists').on('click', '.incomplete_dist', function(){
-		if ( $('.recommended_course').css('display') == 'none' )
+			element = event.target.id;
+			element = "#" + element;
+			alert(element);
+			if ($(element).css('display') == 'none' )
+				$(element).css('display', 'block');
+			else
+				$(element).css('display', 'none');
+				
+		/*if ( $('.recommended_course').css('display') == 'none' )
     		$('.recommended_course').css('display','block');
   		else
-    		$('.recommended_course').css('display','none');
+    		$('.recommended_course').css('display','none'); */
 	//	$('#completed_dists').click(function() {
         });
 
