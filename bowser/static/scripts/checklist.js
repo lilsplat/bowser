4ad67599a20c5c3ebdad7a44f027b63ca4d9c976
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-08-14 09:15:08 -0400

    Added toggle functionality for divs

--- bowser/static/scripts/checklist.js
+++ bowser/static/scripts/checklist.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 	$('#completed_dists').on('click', '.completed_dist', function(){
-		$(".course_title").css("display", "block");
+		if ( $('.course_title').css('display') == 'none' )
+    		$('.course_title').css('display','block');
+  		else
+    		$('.course_title').css('display','none');
 	//	$('#completed_dists').click(function() {
         });
+	$('#incomplete_dists').on('click', '.incomplete_dist', function(){
+		if ( $('.recommended_course').css('display') == 'none' )
+    		$('.recommended_course').css('display','block');
+  		else
+    		$('.recommended_course').css('display','none');
+	//	$('#completed_dists').click(function() {
+        });
+
+
+
+
 });
