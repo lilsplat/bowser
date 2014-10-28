cb2f30d90ec6798c72d5a826be7e152093ad1946
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-10-28 16:46:17 -0400

    Changed settings file

--- bowser/settings.py
+++ bowser/settings.py
@@ -2,11 +2,12 @@
 import os.path
 # from django.contrib import admin
 # from bowser.courses.models import *
+import dj_database_url
 
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = False
+TEMPLATE_DEBUG = False
 PROJECT_PATH = os.path.join(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ADMINS = (
 	('Lilian Xie', 'lxie@wellesley.edu'),
@@ -14,10 +15,11 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+
 """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bowser_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
@@ -26,6 +28,13 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+"""
+#DATABASES = {}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES['default'] = dj_database_url.config()
+
+
+#For AWS
 """
 DATABASES = {
     'default': {
@@ -37,9 +46,10 @@ DATABASES = {
         'PORT': os.environ['RDS_PORT'],
     }
 }
+"""
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -77,7 +87,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'staticfiled'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -88,7 +98,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+#    os.path.join(PROJECT_PATH, 'static'),
+	os.path.join(BASE_DIR, 'static'),
 )
 
 
@@ -188,6 +199,11 @@ LOGGING = {
         },
     }
 }
+
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
