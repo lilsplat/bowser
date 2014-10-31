08fffdca2385ebec1dd74e05fdc0b509ebd2065e
Sravanti Tekumalla <stekumal@wellesley.edu>
2014-10-31 16:26:38 -0400

    Changed the database backend to postgresql

--- bowser/settings.py
+++ bowser/settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'd4v4sjm1l9mavq',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'ungbojjicltral',
